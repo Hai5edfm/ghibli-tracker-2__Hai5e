3a6d822c09ded9722d1762ff4e726fae5dc593b2
Ivan <ivangm180@gmail.com>
2022-08-31 15:15:47 -0500

    [ADD] Improving UX of component and its dimensions

--- components/movies/TopMovies.tsx
+++ components/movies/TopMovies.tsx
@@ -15,20 +15,23 @@ const TopMovies = () => {
     .slice(17)
     .reverse();
   return (
-    <>
+    <div className="md:w-[80%] w-full">
       <h1 className="px-2 m-4 text-2xl font-normal align-middle border-l-4 border-ghibli-dark">
         Top Rated
       </h1>
-      <div className="m-4 overflow-hidden overflow-x-auto bg-white rounded-md h-fit shadow-aesthetic">
-        <ul className="flex flex-row w-[1600px] ">
+      <div className="overflow-hidden overflow-x-auto bg-white rounded-md h-fit shadow-aesthetic">
+        <ul className="flex flex-row ">
           {myArray.length > 0 &&
             myArray.map((item: any) => (
-              <li key={item.id} className="relative w-auto m-4 transition-transform cursor-pointer hover:scale-110">
+              <li
+                key={item.id}
+                className="relative m-4 transition-transform cursor-pointer hover:scale-110"
+              >
                 <img
                   src={item.movie_banner}
                   height={180}
                   width={292}
-                  className="rounded-md"
+                  className="rounded-md max-w-none"
                 />
                 <div className="absolute bottom-0 flex items-end w-full h-full transition-shadow rounded-b-md inner-shadow-bottom hover:inner-shadow-max ">
                   <h1 className="w-40 p-2 font-bold text-left text-gray-100">
@@ -39,7 +42,7 @@ const TopMovies = () => {
             ))}
         </ul>
       </div>
-    </>
+    </div>
   );
 };
 
